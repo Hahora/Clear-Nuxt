d2419de6a4ec7681c7d129a245e47b3b33129bbf
Hahora <bogdandaliba@gmail.com>
2026-05-18 14:44:29 +0300

    fix: remove fatal:true from 404 catch-all to reduce dev noise

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const route = useRoute()
 if (!/\.[a-z0-9]+$/i.test(route.path)) {
-  throw createError({ statusCode: 404, fatal: true })
+  throw createError({ statusCode: 404, statusMessage: 'Страница не найдена' })
 }
 </script>
 
