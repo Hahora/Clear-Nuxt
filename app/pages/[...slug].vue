4981d44bc685ae913b88829de4bd6c4451b1da04
Hahora <bogdandaliba@gmail.com>
2026-05-18 14:42:05 +0300

    fix: skip 404 catch-all for static asset paths

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-throw createError({ statusCode: 404, fatal: true })
+const route = useRoute()
+if (!/\.[a-z0-9]+$/i.test(route.path)) {
+  throw createError({ statusCode: 404, fatal: true })
+}
 </script>
 
 <template><div /></template>
