20387d38ded7706fd1de13008c724cb5c00dc9ba
Hahora <bogdandaliba@gmail.com>
2026-05-18 14:32:46 +0300

    fix: bundle MDI icons offline to eliminate CDN fetch delay

---
+++ app/plugins/iconify.ts
@@ -0,0 +1,6 @@
+import { addCollection } from '@iconify/vue'
+import mdi from '@iconify-json/mdi/icons.json'
+
+export default defineNuxtPlugin(() => {
+  addCollection(mdi as Parameters<typeof addCollection>[0])
+})
